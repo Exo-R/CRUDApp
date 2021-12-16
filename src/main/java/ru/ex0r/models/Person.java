7e1032b33ac16e529f6379c49ca563b845ea9b9a
Exo-R <84266445+Exo-R@users.noreply.github.com>
2021-12-17 03:22:44 +0700

    Update Person.java

--- src/main/java/ru/ex0r/models/Person.java
+++ src/main/java/ru/ex0r/models/Person.java
@@ -4,7 +4,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
 
     private int id;
@@ -18,46 +26,4 @@ public class Person {
 
     @Min(value = 0, message = "Age should be more 0")
     private int age;
-
-    public Person(){
-    }
-
-    public Person(int id, String name, String email, int age) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
